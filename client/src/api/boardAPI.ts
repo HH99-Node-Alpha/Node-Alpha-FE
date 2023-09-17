e4d60b7064caebe92f718e21b9e20fd0880e6598
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-17 15:08:52 +0900

    Refactor: Separate boardAPI

---
+++ client/src/api/boardAPI.ts
@@ -0,0 +1,33 @@
+import { postAPI, getAPI } from "../axios";
+import { TColumn } from "../types/dnd";
+
+export const addNewColumnAPI = async (
+  workspaceId: string,
+  boardId: string,
+  columnName: string
+): Promise<TColumn | null> => {
+  try {
+    const result = await postAPI(
+      `/api/workspaces/${workspaceId}/boards/${boardId}/columns`,
+      {
+        columnName,
+      }
+    );
+    return result.data.column;
+  } catch (error) {
+    console.error("Error adding new column:", error);
+    return null;
+  }
+};
+
+export const getColumnsAPI = async (workspaceId: string, boardId: string) => {
+  try {
+    const result = await getAPI(
+      `/api/workspaces/${workspaceId}/boards/${boardId}/columns`
+    );
+    return result.data.columns;
+  } catch (error) {
+    console.error("Error fetching columns:", error);
+    return [];
+  }
+};
