52f1e2a7f1ddddf1d19a3be934c544ce0db74bec
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-17 15:09:27 +0900

    Setting: Setting react-query for project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,25 @@
+import { QueryClient, QueryClientProvider } from "react-query";
 import Router from "./shared/Router";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      retryDelay: (attempt) => Math.min(attempt * 1000, 30 * 1000),
+      refetchOnWindowFocus: false,
+      onError: (error) => {
+        console.error(error);
+      },
+    },
+  },
+});
 
 function App() {
   return (
     <>
-      <Router />
+      <QueryClientProvider client={queryClient}>
+        <Router />
+      </QueryClientProvider>
     </>
   );
 }
