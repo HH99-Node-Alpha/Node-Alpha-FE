700425d7fb2d67235bb822a26635c7cd259beea5
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-21 10:09:57 +0900

    Feat: Implement search user modal

--- client/src/components/UserSearchModal.tsx
+++ client/src/components/UserSearchModal.tsx
@@ -1,14 +1,34 @@
+import { useState } from "react";
 import { BsSearch } from "react-icons/bs";
+import { useQuery } from "react-query";
+import { getAPI } from "../axios";
 
 interface UserSearchModalProps {
   userSearchModalRef: any;
   closeUserSearchModal: () => void;
 }
 
+const fetchUsers = async (queryKey: any) => {
+  const query = queryKey.queryKey[1];
+  const result = await getAPI(`/api/users/search?email=${query}&name=${query}`);
+  console.log(result);
+  return result.data;
+};
+
 const UserSearchModal: React.FC<UserSearchModalProps> = ({
   userSearchModalRef,
   closeUserSearchModal,
 }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const { data, isError, isLoading } = useQuery(
+    ["searchUsers", searchTerm],
+    fetchUsers,
+    {
+      enabled: searchTerm.length > 0,
+    }
+  );
+
   return (
     <div
       className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-60"
@@ -23,11 +43,27 @@ const UserSearchModal: React.FC<UserSearchModalProps> = ({
           <button onClick={closeUserSearchModal}>X</button>
         </div>
         <div className="flex justify-between px-2 gap-4 mt-3 items-center">
-          <input className="w-full h-8 border-2 rounded-md border-black px-2" />
+          <input
+            className="w-full h-8 border-2 rounded-md border-black px-2"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
           <button>
             <BsSearch />
           </button>
         </div>
+        {isLoading && <div>Loading...</div>}
+        {isError && <div>검색도중 오류가 발생했습니다.</div>}
+        {data && (
+          <ul>
+            {data.map((user: any) => (
+              <div key={user.id} className="flex flex-col px-3">
+                <li>{user.email}</li>
+                <li>{user.name}</li>
+              </div>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
