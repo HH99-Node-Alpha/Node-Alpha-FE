80107f43542d87aaa4ed4f9b017a3969949e320d
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-23 18:21:41 +0900

    Feat: Add alarm count state to notify alarm count

--- client/src/states/userInfoState.ts
+++ client/src/states/userInfoState.ts
@@ -11,3 +11,8 @@ export const inviteResultsState = atom<InviteResult[]>({
   key: "inviteResultsState",
   default: [],
 });
+
+export const alarmCountState = atom<number>({
+  key: "alarmCountState",
+  default: 0,
+});
