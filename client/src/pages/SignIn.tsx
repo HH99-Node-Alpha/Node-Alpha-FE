95e81f902294c93080b8fd341a0676e45e8178b9
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-27 14:46:18 +0900

    Chore: Check React app server url

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -28,7 +28,6 @@ const SignIn: FC = () => {
     event.preventDefault();
     try {
       const result = await postAPI("/api/login", signinRequest);
-      console.log(result);
       localStorage.setItem(
         "user",
         JSON.stringify({
